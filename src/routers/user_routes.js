5e2457140328c90bb0f0c6859011b2ed0ec2d2e9
Arda999-prog <ardannyromero@gmail.com>
2026-03-05 13:16:11 -0500

    Error en ruta

--- src/routers/user_routes.js
+++ src/routers/user_routes.js
@@ -11,9 +11,8 @@ import { verifyAuth } from '../middlewares/auth.js';
 const router = Router();
 router.use(verifyAuth);
 
-// Rutas privadas (Frontend compatibility)
 router.get('/perfil', getProfile);
-router.put('/estudiante/actualizarpassword', updatePassword); // Mantenemos la URL rara por compatibilidad
+router.put('/estudiante/actualizarpassword', updatePassword); 
 router.put('/estudiante/:id', updateProfile);
 router.put('/preferencias', updatePreferences);
 router.post('/actualizar-imagen', updateImage);
